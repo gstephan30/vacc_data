f414b214437b3c4063c851330a46e77367a7fc74
gstephan30 <gstephan30@gmail.com>
2021-02-22 18:37:54 +0100

    20210222 data push

--- push.bat
+++ push.bat
@@ -1,3 +1,4 @@
+:loop
 
 ## setup variable
 set jetzt=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
@@ -9,3 +10,7 @@ git pull
 git add .
 git commit -m "%jetzt% data push"
 git push -u origin master
+
+# repeat
+timeout 86400
+goto loop
--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-jetzt=`date +%Y%m%d`
-
-echo %jetzt%
