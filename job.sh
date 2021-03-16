ff177df868dcbf0b05b19043e5ec032936f1cd9c
stephan <gstephan30@gmail.com>
2021-03-16 06:59:11 +0000

    setup on rpi

---
+++ job.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+depdir=/home/pi/vacc_data
+jetzt=`date +%Y%m%d`
+cd vacc_data
+
+git pull
+
+sudo Rscript download.R
+
+git add . 
+git commit -m "new render task at $jetzt"
+git push -u origin master
+
+cd ..
